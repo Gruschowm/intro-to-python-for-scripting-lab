e587ea8012f27bddf62acac72052fba1b9df6aa0
Gruschowm <markgruschow@outlook.com>
2026-03-10 19:15:41 -0500

    Added control_flow script

---
+++ control-flow.py
@@ -0,0 +1,31 @@
+# Problem 0: Example
+def print_greeting():
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+print_greeting()
+
+# Problem 1: Vowel or consonant
+def check_letter():
+    # Your control flow logic goes here
+    pass # Remove 'pass' when you add your code
+
+# Problem 2: Old enough to vote?
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    pass
+
+# Problem 3: Calculate dog years
+def calculate_dog_years():
+    # Your control flow logic goes here
+    pass
+
+# Problem 4: Weather advice
+def weather_advice():
+    # Your control flow logic goes here
+    pass
+
+# Problem 5: What’s the season?
+def determine_season():
+    # Your control flow logic goes here
+    pass
